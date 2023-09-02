2b716ca3f1c6b234ca45ebf8808e406aca2e6f29
Pooyan Khanjankhani <pooyankhan@gmail.com>
2023-09-02 13:01:54 +0330

    add easy

---
+++ codeforces/1742/a.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int cmp(const void *a, const void *b) { return *(int *)a - *(int *)b; }
+
+int main() {
+	int t;
+	scanf("%d", &t);
+	while (t--) {
+		int arr[3];
+		for (int i = 0; i < 3; i++) {
+			scanf("%d", arr + i);
+		}
+
+		qsort(arr, 3, sizeof(arr[0]), cmp);
+		printf("%s\n", (arr[2] == arr[0] + arr[1]) ? "YES" : "NO");
+	}
+}
