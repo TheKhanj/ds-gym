0ce64aafd5815e9a2b0f3d5f922cfe67c23d56f6
Pooyan Khanjankhani <pooyankhan@gmail.com>
2025-11-21 19:06:05 +0330

    fuck me

--- codeforces/584/a.c
+++ codeforces/584/a.c
@@ -3,7 +3,9 @@
 int main() {
 	int n, t;
 	scanf("%d %d", &n, &t);
-	if (t == 10) {
+	if (n == 1 && t == 10)
+		printf("-1\n");
+	else if (t == 10) {
 		for (int i = 0; i < n - 1; ++i)
 			printf("1");
 		printf("0\n");
